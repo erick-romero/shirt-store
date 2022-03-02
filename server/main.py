43442b035722e046e450c27f3076885f91643caa
fanyCaz <stepheniecaz@gmail.com>
2022-03-01 21:50:30 -0600

    update requirements and connect to firebase

--- server/main.py
+++ server/main.py
@@ -1,9 +1,24 @@
+import os
 from typing import Optional
 
 from fastapi import FastAPI
+from dotenv import load_dotenv
+
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
 
 app = FastAPI()
 
+load_dotenv()
+
+
+credential = credentials.ApplicationDefault()
+firebase_admin.initialize_app(credential, {'projectId': os.environ.get('IDPROJECT')})
+
+db = firestore.client()
+
+print(db)
 
 @app.get("/")
 def read_root():
@@ -13,3 +28,8 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
+
+@app.get("/products/")
+def get_products():
+    products = {"id": "prod1", "name": "Camiseta 1"}
+    return products
