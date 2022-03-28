52ad994706d31c564ae490a1d360a966fc81d4e8
erick-romero <erick.romeroar@gmail.com>
2022-03-27 21:20:19 -0600

    remove creds?

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,5 +1,12 @@
 export const environment = {
   firebase: {
+    projectId: 'shirt-store-28395',
+    appId: '1:258453908537:web:91c098a2c4db39c2e9b1e8',
+    databaseURL: 'https://shirt-store-28395-default-rtdb.firebaseio.com',
+    storageBucket: 'shirt-store-28395.appspot.com',
+    apiKey: 'AIzaSyCJJ1gimhk1RXf77mr4E_EG214spYfUCGY',
+    authDomain: 'shirt-store-28395.firebaseapp.com',
+    messagingSenderId: '258453908537',
   },
   production: true
 };
